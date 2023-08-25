fc4aba3cccc3726017b7bd867d716f99a05bbea2
sumoyang123 <khs123153@gmail.com>
2023-08-25 10:40:44 +0900

    uptdate

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    /* 섹션02 슬라이드 */
+    var swiper = new Swiper('.mySwiper', {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        loop: true,
+        loopedSlides: 1,
+        // autoplay: {
+        //     delay: 3000,
+        //     disableOnInteraction: false,
+        // },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.1,
+                loopedSlides: 1.1,
+                spaceBetween: 16,
+            },
+            500: {
+                slidesPerView: 2.3,
+                loopedSlides: 2.3,
+                spaceBetween: 16,
+            },
+            1024: {
+                slidesPerView: 2.5,
+                loopedSlides: 2.5,
+                spaceBetween: 16,
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
